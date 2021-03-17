b031d82da568b51865f899ba436e22fa2aa61f75
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-03-17 13:31:04 +0700

    add docs

--- mix.exs
+++ mix.exs
@@ -25,11 +25,9 @@ defmodule Magik.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
       {:ecto, "~> 3.0", optional: true},
-      {:plug, "~> 1.11", optional: true}
+      {:plug, "~> 1.11", optional: true},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
